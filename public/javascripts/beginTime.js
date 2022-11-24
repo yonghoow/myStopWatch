a1cf804580e58bcc5c95c7e767ff687172111827
yonghoow <2073951b@student.tp.edu.sg>
2022-11-24 20:30:51 +0800

    commit on Nov 24

--- public/javascripts/beginTime.js
+++ public/javascripts/beginTime.js
@@ -2,6 +2,7 @@ var d;
 var beginTime;
 var beginHours;
 var beginMins;
+var beginSecs;
 var beginDate;
 var beginMonth;
 var beginYear;
@@ -11,8 +12,12 @@ function displayTime() {
     beginTime = d.getTime();
     beginHours = d.getHours();
     beginMins = d.getMinutes();
+    beginMins = beginMins.toString().padStart(2, '0');
+    beginSecs = d.getSeconds();
+    beginSecs = beginSecs.toString().padStart(2, '0');
     beginDate = d.getDate();
     beginMonth = d.getMonth() + 1;
     beginYear = d.getFullYear();
-    document.getElementById("demo").innerHTML = "Scan passport on: " + beginDate + "/" + beginMonth + "/" + beginYear + " at " + beginHours + ":" + beginMins + " hrs";
+    document.getElementById("demo").innerHTML = "Scan passport on: " + beginDate + "/" + beginMonth + "/" + beginYear;
+    document.getElementById("demo1").innerHTML = " at " + beginHours + ":" + beginMins + ":" + beginSecs + " (h:min:sec)";
 }
